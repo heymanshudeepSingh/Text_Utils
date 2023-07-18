76852d454b2bfb2ccf8cd63a697bd2227821fb63
Simar <heymanshudeepsingh@yahoo.com>
2023-07-17 23:54:13 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+let name = "Simar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <nav>
+   <li>Home</li>
+   <li>About</li>
+   <li>References</li>
+   </nav>
+   <div className="container">
+    <h1>Hello {name}</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Reprehenderit facere harum facilis mollitia unde ratione error perferendis, 
+     laudantium obcaecati tempore, ut impedit praesentium voluptates asperiores quasi debitis? Eligendi, facilis excepturi.
+   </p>
+   </div>
+   </>
   );
 }
 
